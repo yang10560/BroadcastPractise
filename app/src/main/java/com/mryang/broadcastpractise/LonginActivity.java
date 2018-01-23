aa2aedc18156e9fd625b10e68a7951b4d8efb3f8
mryang <yang10560@163.com>
2018-01-23 12:53:36 +0800

    Android广播测试

--- app/src/main/java/com/mryang/broadcastpractise/LonginActivity.java
+++ app/src/main/java/com/mryang/broadcastpractise/LonginActivity.java
@@ -1,31 +1,66 @@
 package com.mryang.broadcastpractise;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.prefs.PreferenceChangeEvent;
 
 public class LonginActivity extends BaseActivity {
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button loginButton;
 
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_longin);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass = (CheckBox)findViewById(R.id.remember_pass);
         accountEdit = (EditText)findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         loginButton = (Button) findViewById(R.id.login);
+
+        //读取密码。
+        boolean isRemember = pref.getBoolean("remember_pass",false);
+        if(isRemember){
+            accountEdit.setText(pref.getString("account",""));
+            passwordEdit.setText(pref.getString("password",""));
+            rememberPass.setChecked(true);
+        }
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
                 if(account.equals("admin") && password.equals("admin888")){
+
+                    //保存密码
+                    editor = pref.edit();
+                    if(rememberPass.isChecked()){
+                        editor.putBoolean("remember_pass",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
+
+
+
                     Intent intent = new Intent(LonginActivity.this,MainActivity.class);
                     startActivity(intent);
                 }else {
